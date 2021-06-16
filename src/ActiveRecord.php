8afed9ba62a4a6332acd944492b8950da703dd56
lyl <254364502@qq.com>
2021-06-16 17:59:34 +0800

    composer cs-fix

--- src/ActiveRecord.php
+++ src/ActiveRecord.php
@@ -1,12 +1,17 @@
 <?php
 
 declare(strict_types=1);
-
+/**
+ * This file is part of Simps.
+ *
+ * @link     https://simps.io
+ * @document https://doc.simps.io
+ * @license  https://github.com/simple-swoole/simps/blob/master/LICENSE
+ */
 namespace Simps\DB;
 
 /**
- * Class ActiveRecord
- * @package Simps\DB
+ * Class ActiveRecord.
  */
 abstract class ActiveRecord
 {
@@ -21,9 +26,15 @@ abstract class ActiveRecord
     private $_columns;
 
     /**
-     * @var array|null
+     * @var null|array
      */
     private $_oldAttributes;
+
+    /**
+     * 主键.
+     * @var null
+     */
+    private static $_primaryKey = null;
 
     /**
      * ActiveRecord constructor.
@@ -34,7 +45,7 @@ abstract class ActiveRecord
     }
 
     /**
-     * getIsNewRecord
+     * getIsNewRecord.
      * @return bool
      */
     public function getIsNewRecord()
@@ -43,7 +54,7 @@ abstract class ActiveRecord
     }
 
     /**
-     * getColumns
+     * getColumns.
      * @return array
      */
     public function getColumns()
@@ -52,38 +63,22 @@ abstract class ActiveRecord
     }
 
     /**
-     * getOldAttributes
+     * getOldAttributes.
      * @param null $column
-     * @return array|mixed|null
+     * @return null|array|mixed
      */
-    public function getOldAttributes($column=null)
+    public function getOldAttributes($column = null)
     {
-        return $column?($this->_oldAttributes[$column]??null):$this->_oldAttributes;
+        return $column ? ($this->_oldAttributes[$column] ?? null) : $this->_oldAttributes;
     }
 
     /**
-     * findTableColumns
-     */
-    protected function findTableColumns()
-    {
-        foreach (static::getTableSchema() as $column) {
-
-            $field = $column['Field'];
-
-            $this->{$field} = $column['Default'];
-
-            $this->_columns[$field] = $column;
-        }
-    }
-
-    /**
-     * tableName
-     * @return string
+     * tableName.
      */
     abstract public static function tableName(): string;
 
     /**
-     * getDb
+     * getDb.
      * @return BaseModel
      */
     public static function getDb()
@@ -92,42 +87,37 @@ abstract class ActiveRecord
     }
 
     /**
-     * getTableSchema
+     * getTableSchema.
      * @return array
      */
     public static function getTableSchema()
     {
         static $_columns;
-        if(empty($_columns)){
+        if (empty($_columns)) {
             $_columns = static::getDb()->query('SHOW FULL COLUMNS FROM ' . static::tableName())->fetchAll();
         }
         return $_columns;
     }
 
     /**
-     * 主键
-     * @var null
-     */
-    private static $_primaryKey = null;
-
-    /**
      * 主键必须设置
-     * getPrimaryKey
+     * getPrimaryKey.
      * @return array
      */
     public static function getPrimaryKey()
     {
-        if(empty(self::$_primaryKey)){
+        if (empty(self::$_primaryKey)) {
             foreach (static::getTableSchema() as $column) {
-                if ($column['Key'] == 'PRI')
+                if ($column['Key'] == 'PRI') {
                     self::$_primaryKey[] = $column['Field'];
+                }
             }
         }
         return self::$_primaryKey;
     }
 
     /**
-     * toArray
+     * toArray.
      * @return array
      */
     public function toArray()
@@ -142,25 +132,7 @@ abstract class ActiveRecord
     }
 
     /**
-     * createModelsByDataBase
-     * @param array $rows
-     * @return $this
-     */
-    protected function createModelsByDataBase(array $rows)
-    {
-
-        foreach ($rows as $column => $value) {
-            $this->{$column}               = $value;
-            $this->_oldAttributes[$column] = $value;
-        }
-
-        $this->_isNewRecord = false;
-
-        return $this;
-    }
-
-    /**
-     * getChangeAttributes
+     * getChangeAttributes.
      * @return array
      */
     public function getChangeAttributes()
@@ -175,7 +147,199 @@ abstract class ActiveRecord
     }
 
     /**
-     * getPrimaryKeyCondition
+     * save.
+     * @return bool
+     */
+    public function save()
+    {
+        return $this->getIsNewRecord() ? $this->insert() : $this->update();
+    }
+
+    /**
+     * update.
+     * @return bool
+     */
+    public function update()
+    {
+        $changeAttributes = $this->getChangeAttributes();
+        if (! empty($changeAttributes)) {
+            return static::getDb()->update(static::tableName(), $changeAttributes, $this->getPrimaryKeyCondition())->rowCount();
+        }
+        return true;
+    }
+
+    /**
+     * insert.
+     * @return bool
+     */
+    public function insert()
+    {
+        $insertId = static::getDb()->insert(static::tableName(), $this->toArray());
+
+        $primaryKey = self::getPrimaryKey()[0] ?? null;
+
+        if ($primaryKey && isset($this->{$primaryKey})) {
+            $this->{$primaryKey} = $insertId;
+        }
+
+        return $insertId ? true : false;
+    }
+
+    /**
+     * delete.
+     * @return bool
+     */
+    public function delete()
+    {
+        return static::getDb()->delete(static::tableName(), $this->getPrimaryKeyCondition())->rowCount() ? true : false;
+    }
+
+    /**
+     * findOrCreate.
+     * @param array|string $where
+     * @return ActiveRecord|static
+     */
+    public static function findOrCreate($where)
+    {
+        $where = self::formatWhere($where);
+
+        $data = null;
+
+        if (! empty($where)) {
+            $data = static::getDb()->get(static::tableName(), '*', self::formatWhere($where));
+        }
+
+        $model = (new static());
+        if (! empty($data)) {
+            return $model->createModelsByDataBase($data);
+        }
+
+        return $model;
+    }
+
+    /**
+     * findOne.
+     * @param array|string $where
+     * @return static
+     */
+    public static function findOne($where)
+    {
+        $where = self::formatWhere($where);
+
+        if (empty($where)) {
+            return null;
+        }
+
+        $data = static::getDb()->get(static::tableName(), '*', $where);
+        return $data ? (new static())->createModelsByDataBase($data) : null;
+    }
+
+    /**
+     * findAll.
+     * @param array $where
+     * @return array|static[]
+     */
+    public static function findAll($where = [])
+    {
+        $dataList = static::getDb()->select(static::tableName(), '*', $where);
+
+        if (empty($dataList)) {
+            return [];
+        }
+
+        $models = [];
+
+        foreach ($dataList as $data) {
+            $models[] = (new static())->createModelsByDataBase($data);
+        }
+
+        return $models;
+    }
+
+    /**
+     * insertOne.
+     * @return bool
+     */
+    public static function insertOne(array $data)
+    {
+        return static::getDb()->insert(static::tableName(), [$data])->rowCount();
+    }
+
+    /**
+     * insertMulti.
+     * @return bool
+     */
+    public static function insertMulti(array $data)
+    {
+        return static::getDb()->insert(static::tableName(), $data)->rowCount();
+    }
+
+    /**
+     * updateAll.
+     * @param array $where
+     * @return bool
+     */
+    public static function updateAll(array $data, $where = [])
+    {
+        return static::getDb()->update(static::tableName(), $data, $where)->rowCount();
+    }
+
+    /**
+     * deleteAll.
+     * @param null $where
+     * @return bool
+     */
+    public static function deleteAll($where = null)
+    {
+        return static::getDb()->delete(static::tableName(), $where)->rowCount() ? true : false;
+    }
+
+    /**
+     * load.
+     */
+    public function load(array $data)
+    {
+        foreach ($data as $key => $value) {
+            if (! isset($this->{$key})) {
+                continue;
+            }
+
+            $this->{$key} = $value;
+        }
+    }
+
+    /**
+     * findTableColumns.
+     */
+    protected function findTableColumns()
+    {
+        foreach (static::getTableSchema() as $column) {
+            $field = $column['Field'];
+
+            $this->{$field} = $column['Default'];
+
+            $this->_columns[$field] = $column;
+        }
+    }
+
+    /**
+     * createModelsByDataBase.
+     * @return $this
+     */
+    protected function createModelsByDataBase(array $rows)
+    {
+        foreach ($rows as $column => $value) {
+            $this->{$column} = $value;
+            $this->_oldAttributes[$column] = $value;
+        }
+
+        $this->_isNewRecord = false;
+
+        return $this;
+    }
+
+    /**
+     * getPrimaryKeyCondition.
      * @return array
      */
     protected function getPrimaryKeyCondition()
@@ -190,178 +354,15 @@ abstract class ActiveRecord
     }
 
     /**
-     * save
-     * @return bool
-     */
-    public function save()
-    {
-        return $this->getIsNewRecord() ? $this->insert() : $this->update();
-    }
-
-    /**
-     * update
-     * @return bool
-     */
-    public function update()
-    {
-        $changeAttributes = $this->getChangeAttributes();
-        if(!empty($changeAttributes)){
-            return static::getDb()->update(static::tableName(), $changeAttributes, $this->getPrimaryKeyCondition())->rowCount();
-        }
-        return true;
-    }
-
-    /**
-     * insert
-     * @return bool
-     */
-    public function insert()
-    {
-        $insertId = static::getDb()->insert(static::tableName(), $this->toArray());
-
-        $primaryKey = self::getPrimaryKey()[0]??null;
-
-        if($primaryKey && isset($this->{$primaryKey}))
-            $this->{$primaryKey} = $insertId;
-
-        return $insertId?true:false;
-    }
-
-    /**
-     * delete
-     * @return bool
-     */
-    public function delete()
-    {
-        return static::getDb()->delete(static::tableName(), $this->getPrimaryKeyCondition())->rowCount()?true:false;
-    }
-
-    /**
-     * formatWhere
+     * formatWhere.
      * @param array|string $where
      * @return array
      */
     protected static function formatWhere($where)
     {
-        if(is_string($where)){
-            return [self::getPrimaryKey()[0]=>$where];
+        if (is_string($where)) {
+            return [self::getPrimaryKey()[0] => $where];
         }
         return $where;
     }
-
-    /**
-     * findOrCreate
-     * @param array|string $where
-     * @return ActiveRecord|static
-     */
-    public static function findOrCreate($where)
-    {
-        $where = self::formatWhere($where);
-
-        $data = null;
-
-        if(!empty($where))
-            $data  = static::getDb()->get(static::tableName(), '*', self::formatWhere($where));
-
-        $model = (new static());
-        if (!empty($data)) {
-            return $model->createModelsByDataBase($data);
-        }
-
-        return $model;
-    }
-
-    /**
-     * findOne
-     * @param array|string $where
-     * @return static
-     */
-    public static function findOne($where)
-    {
-        $where = self::formatWhere($where);
-
-        if(empty($where))
-            return null;
-
-        $data = static::getDb()->get(static::tableName(), '*', $where);
-        return $data ? (new static())->createModelsByDataBase($data) : null;
-    }
-
-    /**
-     * findAll
-     * @param array $where
-     * @return static[]|array
-     */
-    public static function findAll($where = [])
-    {
-        $dataList = static::getDb()->select(static::tableName(), '*', $where);
-
-        if (empty($dataList))
-            return [];
-
-        $models = [];
-
-        foreach ($dataList as $data) {
-            $models[] = (new static())->createModelsByDataBase($data);
-        }
-
-        return $models;
-    }
-
-    /**
-     * insertOne
-     * @param array $data
-     * @return bool
-     */
-    public static function insertOne(array $data)
-    {
-        return static::getDb()->insert(static::tableName(), [$data])->rowCount();
-    }
-
-    /**
-     * insertMulti
-     * @param array $data
-     * @return bool
-     */
-    public static function insertMulti(array $data)
-    {
-        return static::getDb()->insert(static::tableName(), $data)->rowCount();
-    }
-
-    /**
-     * updateAll
-     * @param array $data
-     * @param array $where
-     * @return bool
-     */
-    public static function updateAll(array $data, $where = [])
-    {
-        return static::getDb()->update(static::tableName(), $data, $where)->rowCount();
-    }
-
-    /**
-     * deleteAll
-     * @param null $where
-     * @return bool
-     */
-    public static function deleteAll($where = null)
-    {
-        return static::getDb()->delete(static::tableName(), $where)->rowCount()?true:false;
-    }
-
-    /**
-     * load
-     * @param array $data
-     */
-    public function load(array $data)
-    {
-        foreach ($data as $key => $value) {
-
-            if (!isset($this->{$key}))
-                continue;
-
-            $this->{$key} = $value;
-        }
-    }
-
 }
